b550b30d6c0e95eae2f3a8c8fb51dad971b27384
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-05 10:28:26 +0300

    postioned the start perfectly

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,16 +1,27 @@
 import React, { PropsWithChildren } from "react";
 
-const HeroOrbit = ({ children, size }: PropsWithChildren<{ size: number }>) => {
+const HeroOrbit = ({
+  children,
+  size,
+  rotation,
+}: PropsWithChildren<{ size: number; rotation: number }>) => {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <div
-        className=""
         style={{
+          transform: `rotate(${rotation}deg)`,
           height: `${size}px`,
           width: `${size}px`,
         }}
       >
-        <div className=" inline-flex ">{children}</div>
+        <div
+          className="inline-flex"
+          style={{
+            transform: `rotate(${rotation * -1}deg)`,
+          }}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
