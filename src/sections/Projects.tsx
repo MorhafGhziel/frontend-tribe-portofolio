b639b58c331e01983c99317166bd8fd45898f5b2
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-06 17:17:56 +0300

    mapped over projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -48,6 +48,18 @@ export const ProjectsSection = () => {
         <p>Real-world Results</p>
         <h2>Featured Projects</h2>
         <p>See how I transformed concepts into engaging digital experiences.</p>
+        <div>
+          {portfolioProjects.map((project) => (
+            <>
+              <div key={project.title}></div>
+              <span> {project.company}</span>
+              <span> {project.year}</span>
+              <div>
+                <h3>{project.title}</h3>
+              </div>
+            </>
+          ))}
+        </div>
       </div>
     </div>
   );
