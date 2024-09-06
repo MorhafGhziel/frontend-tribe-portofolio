87e4a9df35a870ef5d667701f021230d81ae08fd
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-06 18:50:05 +0300

    edited the button and made it clickable and the projects page is dont at the mobile view

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -65,7 +65,7 @@ export const ProjectsSection = () => {
           {portfolioProjects.map((project) => (
             <div
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 after:z-10 overflow-hidden after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8"
+              className="bg-gray-800 rounded-3xl relative z-0 after:z-10 overflow-hidden after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 after:pointer-events-none"
             >
               <div
                 className="absolute inset-0 -z-10 opacity-5"
