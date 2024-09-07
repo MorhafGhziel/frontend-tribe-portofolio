a531126dbd661a79715e6d8e3c5931732ac90a30
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-07 22:50:38 +0300

    added rotate to the tape

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -16,7 +16,7 @@ const words = [
 export const TapeSection = () => {
   return (
     <div className="py-16">
-      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 -mx-1">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
           <div className="flex flex-none gap-4 py-3">
             {words.map((word) => (
