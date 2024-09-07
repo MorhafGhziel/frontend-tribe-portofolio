1b128e652cc84e1f3ea87f295fe2f6717f02c9a0
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-07 22:39:36 +0300

    wrapped the map with another div

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -16,13 +16,15 @@ const words = [
 export const TapeSection = () => {
   return (
     <div className="py-16">
-      <div className="flex gap-4">
-        {words.map((word) => (
-          <div key={word} className="inline-flex gap-4">
-            <span>{word}</span>
-            <StartIcon className="size-6" />
-          </div>
-        ))}
+      <div>
+        <div className="flex gap-4">
+          {words.map((word) => (
+            <div key={word} className="inline-flex gap-4">
+              <span>{word}</span>
+              <StartIcon className="size-6" />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
