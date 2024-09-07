cd71595031d8b4d10c1b11681be3dae95cee2959
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-07 22:28:40 +0300

    created the words for the tape

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,16 @@
+const words = [
+  "Perfomant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Maintainable",
+  "Search Optimize",
+  "Usable",
+  "Reliable",
+];
+
 export const TapeSection = () => {
   return <div>Tape Section</div>;
 };
