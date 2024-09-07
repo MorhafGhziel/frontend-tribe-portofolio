2c0c56daa1112cefceedb26e009b2c31ae68c27e
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-07 22:29:40 +0300

    mapped over the words to get every single word

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -12,5 +12,11 @@ const words = [
 ];
 
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div>
+      {words.map((word) => (
+        <div key={word}></div>
+      ))}
+    </div>
+  );
 };
