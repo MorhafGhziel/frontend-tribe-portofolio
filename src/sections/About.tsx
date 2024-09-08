b85b4bea1c10f2f675fe58353a50084da9f60782
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-08 16:41:04 +0300

    created group for the toolboxitems and mapped it

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -3,6 +3,39 @@ import SectionHeader from "@/components/SectionHeader";
 import StarIcon from "@/assets/icons/star.svg";
 import bookImage from "@/assets/images/book-cover.png";
 import Image from "next/image";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+
+const toolboxItems = [
+  {
+    title: "JavaScript",
+    icon: <JavascriptIcon />,
+  },
+  {
+    title: "HTML5",
+    icon: <HTMLIcon />,
+  },
+  {
+    title: "CSS3",
+    icon: <CssIcon />,
+  },
+  {
+    title: "React",
+    icon: <ReactIcon />,
+  },
+  {
+    title: "Chrome",
+    icon: <ChromeIcon />,
+  },
+  {
+    title: "Github",
+    icon: <GithubIcon />,
+  },
+];
 
 export const AboutSection = () => {
   return (
@@ -20,6 +53,21 @@ export const AboutSection = () => {
             <p>Explore the books shaping my perspectives.</p>
           </div>
           <Image src={bookImage} alt="Book Cover" />
+        </Card>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>My Toolbox</h3>
+            <p>
+              Explore the technologies and tools I use to craft exceptional
+              digital experiences.
+            </p>
+          </div>
+          <div>
+            {toolboxItems.map((item) => (
+              <div key={item.title}></div>
+            ))}
+          </div>
         </Card>
       </div>
     </div>
