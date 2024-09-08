229b3cd9f72209f0350ec6e928ba6770f3ba8e4b
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-08 16:26:50 +0300

    added the staricon inside the card

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,13 +1,22 @@
+import Card from "@/components/Card";
 import SectionHeader from "@/components/SectionHeader";
+import StarIcon from "@/assets/icons/star.svg";
 
 export const AboutSection = () => {
   return (
-    <div className="">
+    <div className="pb-96">
       <SectionHeader
-        title="ABOUT ME"
+        title="About Me"
         eyebrow="A Glimpse Into My World"
         description="Learn more about who i am, what i do, what inspires me"
       />
+      <div>
+        <Card>
+          <div>
+            <StarIcon />
+          </div>
+        </Card>
+      </div>
     </div>
   );
 };
