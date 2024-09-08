41143ebbf5119774c9e93857939170dff7a8efe7
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-08 13:49:27 +0300

    updated the tape margin

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -17,7 +17,7 @@ const words = [
 export const TapeSection = () => {
   return (
     <div className="py-16 lg:py-24 overflow-x-clip">
-      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 mx-[1.3px]">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
           <div className="flex flex-none gap-4 py-3">
             {words.map((word) => (
@@ -25,7 +25,7 @@ export const TapeSection = () => {
                 <span className="text-gray-900 uppercase font-extrabold text-sm">
                   {word}
                 </span>
-                <StartIcon className="size-6 text-gray-900 -rotate-12" />
+                <StartIcon className="size-6 text-gray-900 -rotate-3" />
               </div>
             ))}
           </div>
