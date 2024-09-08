de859009cfeb2b5df962830d08f50a39ea1669ad
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-08 16:01:52 +0300

    fixed the card componenet and made the cards next to each other

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -51,27 +51,29 @@ export const TestimonialsSection = () => {
           description="Don't just take my word for it. See what my clients have to say
         about my work."
         />
-        <div>
-          {testimonials.map((testimonial) => (
-            <Card key={testimonial.name}>
-              <div className="flex gap-4">
-                <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center">
-                  <Image
-                    src={testimonial.avatar}
-                    alt={testimonial.name}
-                    className="max-h-full"
-                  />
-                </div>
-                <div>
-                  <div className="font-semibold">{testimonial.name}</div>
-                  <div className="text-sm text-white/40">
-                    {testimonial.position}
+        <div className="mt-16 flex">
+          <div className="flex gap-8 flex-none">
+            {testimonials.map((testimonial) => (
+              <Card key={testimonial.name} className="max-w-xs">
+                <div className="flex gap-4 items-center">
+                  <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center">
+                    <Image
+                      src={testimonial.avatar}
+                      alt={testimonial.name}
+                      className="max-h-full"
+                    />
+                  </div>
+                  <div>
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">
+                      {testimonial.position}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <p className="mt-4 text-sm">{testimonial.text}</p>
-            </Card>
-          ))}
+                <p className="mt-4 text-sm">{testimonial.text}</p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
