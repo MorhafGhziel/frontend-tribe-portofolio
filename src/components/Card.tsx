9ce2e428fe73845ca9dc03c8cdb668fb9930bf9e
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-08 15:00:58 +0300

    costumized the componenet

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
+import grainImage from "@/assets/images/grain.jpg";
+import { twMerge } from "tailwind-merge";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({
+  className,
+  children,
+}: PropsWithChildren<{ className?: string }>) => {
+  return (
+    <div
+      className={twMerge(
+        "bg-gray-800 rounded-3xl relative z-0 after:z-10 overflow-hidden after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 p-6 after:pointer-events-none"
+      )}
+    >
+      <div
+        className="absolute inset-0 -z-10 opacity-5"
+        style={{
+          backgroundImage: `url(${grainImage.src})`,
+        }}
+      ></div>
+    </div>
+  );
 };
 
 export default Card;
