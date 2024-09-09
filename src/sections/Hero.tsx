ff3024c16a18c32463f0d97ba06553fb3e10188a
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-09 18:49:42 +0300

    added ping animation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -60,7 +60,9 @@ export const HeroSection = () => {
             alt="Person peeking from behind laptop"
           />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-            <div className="bg-green-500 rounded-full size-2.5"></div>
+            <div className="bg-green-500 rounded-full size-2.5 relative">
+              <div className="bg-green-500 inset-0 rounded-full absolute animate-ping-large"></div>
+            </div>
             <div className="text-sm font-medium">
               Available for new projects
             </div>
