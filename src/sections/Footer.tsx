358f751389c28f07a86ee68e2ef8c7b439527d69
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-09 17:54:58 +0300

    done styling

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -19,13 +19,17 @@ export const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <div>
-          <div>&copy; 2024. All rights reserved.</div>
-          <nav>
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col gap-8 items-center">
+          <div className="text-white/40">&copy; 2024. All rights reserved.</div>
+          <nav className="flex flex-col items-center gap-8">
             {footerLinks.map((link) => (
-              <a href="#" key={link.title}>
-                <span>{link.title}</span>
-                <ArrowUpRight />
+              <a
+                href="#"
+                key={link.title}
+                className="inline-flex items-center gap-1.5"
+              >
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRight className="size-4" />
               </a>
             ))}
           </nav>
