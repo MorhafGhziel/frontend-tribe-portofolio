d0328fec4f8a98730b909ac9afbb5f3fde799824
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-10 14:10:11 +0300

    created custom animation

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -20,7 +20,7 @@ export const TapeSection = () => {
     <div className="py-16 lg:py-24 overflow-x-clip">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-4 py-3">
+          <div className="flex flex-none gap-4 pr-4 py-3 -translate-x-1/2">
             {[...new Array(2)].fill(0).map((_, idx) => (
               <Fragment key={idx}>
                 {words.map((word) => (
@@ -28,7 +28,7 @@ export const TapeSection = () => {
                     <span className="text-gray-900 uppercase font-extrabold text-sm">
                       {word}
                     </span>
-                    <StartIcon className="size-6 text-gray-900 -rotate-3" />
+                    <StartIcon className="size-6 text-gray-900 -rotate-12" />
                   </div>
                 ))}
               </Fragment>
