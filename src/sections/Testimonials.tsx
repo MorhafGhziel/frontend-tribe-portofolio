9751c1562998c15de90e7396ea6e4df04bf81ff8
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-10 14:27:40 +0300

    added move-left animation

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -53,7 +53,7 @@ export const TestimonialsSection = () => {
         about my work."
         />
         <div className="mt-12 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] py-4 -my-4">
-          <div className="flex gap-8 flex-none">
+          <div className="flex gap-8 flex-none animate-move-left [animation-duration:50s]">
             {[...new Array(2)].fill(0).map((_, idx) => (
               <Fragment key={idx}>
                 {testimonials.map((testimonial) => (
