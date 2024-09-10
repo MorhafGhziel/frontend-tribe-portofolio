e6634f0cc3c9909fa5dfb737b56fa9e0323ee133
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-10 15:15:41 +0300

    added on click project sends u to the section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,13 +1,27 @@
+"use client";
+
 export const Header = () => {
+  const handleScrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         <a href="#" className="nav-item">
           Home
         </a>
-        <a href="#" className="nav-item">
+        <a
+          href="#"
+          className="nav-item"
+          onClick={() => handleScrollToSection("projects")}
+        >
           Projects
         </a>
+
         <a href="#" className="nav-item">
           About
         </a>
